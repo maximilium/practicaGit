39d7e074b6abb2e43c1318d795839b5bfbb4a67f
Alejandro <micorreo@uma.es>
2013-03-11 21:29:21 +0100

    testing en GitAux

--- GitAux.java
+++ GitAux.java
@@ -21,4 +21,10 @@ public class GitAux {
     public void metodoComunitario(){
         System.out.println("Aqui escribimos todos");
     }
+
+    public void testGitAux(){
+     //metodo rama testing 
+     System.out.println("Metodo de testing");
+    }
+
 }
