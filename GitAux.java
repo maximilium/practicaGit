e6f0b3a1a4f96d0bceacd0c29e3033306f8bc513
Alejandro <micorreo@uma.es>
2013-03-11 21:13:46 +0100

    mensaje descriptivo

--- GitAux.java
+++ GitAux.java
@@ -1,12 +1,12 @@
 /*
  * To change this template, choose Tools | Templates
- * and open the template in the editor.
  */
 package prgitejemplo;
 
 /**
  *
- * @author bertoa
+ * @author Alejandro
+ * Diaz Martin
  */
 public class GitAux {
     
---
+++ README.txt
@@ -0,0 +1,2 @@
+Alejandro Díaz Martín 
+Grado de computadores
